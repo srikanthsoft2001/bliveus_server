cd90de7ef945df79ff046663e03534cd65f554cf
srikanth <rmsrikanth25@gmail.com>
2025-05-12 18:34:12 +0530

    simple swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('bliveus API')
+    .setDescription('Bliveus API for Ecommerce Website')
+    .setVersion('1.0')
+    // .addTag('cats')
+    // .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
