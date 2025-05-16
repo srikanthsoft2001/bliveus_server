4228d03711519be889f29ae1d6a858fa874a3700
Sai Prakash Ragipati <saiprakash1404@gmail.com>
2025-05-16 11:43:10 +0530

    some changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,9 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
 import { MongooseModule } from '@nestjs/mongoose';
-<<<<<<< HEAD
+
 // import { UserModule } from './user/user.module';
-=======
->>>>>>> 7e6c7cd866d0ff286ff6115157a8630b498a60e3
+
 import { ProductModule } from './product/product.module';
 
 @Module({
@@ -22,10 +21,8 @@ import { ProductModule } from './product/product.module';
       }),
       inject: [ConfigService],
     }),
-<<<<<<< HEAD
+
     // UserModule,
-=======
->>>>>>> 7e6c7cd866d0ff286ff6115157a8630b498a60e3
     ProductModule,
   ],
   controllers: [AppController],
