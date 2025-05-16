24519f85bf7a2e99ba674050c4a7414c96fb4109
srikanth <rmsrikanth25@gmail.com>
2025-05-16 07:19:54 +0530

    cookieparser issue resolved

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProductModule } from './product/product.module';
--- src/main.ts
+++ src/main.ts
@@ -2,32 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  await app.listen(process.env.PORT || 3000);
-
-  // // Enable CORS for your frontend origin
-  // app.enableCors({
-  //   origin: 'http://localhost:5173/',
-  //   credentials: true,
-  // });
 
   // Swagger setup
   const config = new DocumentBuilder()
     .setTitle('bliveus API')
     .setDescription('Bliveus API for Ecommerce Website')
     .setVersion('1.0')
-    // .addTag('cats')
-    // .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
