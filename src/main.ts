8749369fb2047f6e037c9ab11536086b0929bf0a
srikanth <rmsrikanth25@gmail.com>
2025-05-16 11:39:45 +0530

    cors added

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser());
+
+  // Enable CORS for your frontend origin
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
 
   // Swagger setup
   const config = new DocumentBuilder()
-    .setTitle('bliveus API')
-    .setDescription('Bliveus API for Ecommerce Website')
+    .setTitle('My API')
+    .setDescription('The API description')
     .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
