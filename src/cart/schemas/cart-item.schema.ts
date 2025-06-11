335daadbaaf21d806a9f6536d2fbad16f29e1d47
srikanth <rmsrikanth25@gmail.com>
2025-06-11 10:30:07 +0530

    ---

--- src/cart/schemas/cart-item.schema.ts
+++ src/cart/schemas/cart-item.schema.ts
@@ -1,7 +1,6 @@
 // src/cart/schemas/cart-item.schema.ts
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { SchemaTypes, Types } from 'mongoose';
-import { Product } from '../../product/schemas/product.schema';
 
 @Schema({ _id: false })
 export class CartItem {
@@ -13,12 +12,6 @@ export class CartItem {
 
   @Prop({ required: true })
   price: number;
-
-  @Prop()
-  name?: string; // Optional, can be populated from product
-
-  @Prop()
-  image?: string; // Optional, can be populated from product
 }
 
 export const CartItemSchema = SchemaFactory.createForClass(CartItem);
