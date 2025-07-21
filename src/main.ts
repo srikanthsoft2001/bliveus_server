261abff51a8d410152ccccb0a358fb7b7f19c04a
SanjivaniHake <hakesanjivani@gmail.com>
2025-07-21 15:45:42 +0530

    Update main.ts and added elastic ip

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://13.204.19.250:5173',
     credentials: true,
   });
 
